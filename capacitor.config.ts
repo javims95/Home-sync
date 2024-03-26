23dfa3a34451cd77d8685f26bf5bb0e188794d5b
javims95 <57890567+javims95@users.noreply.github.com>
2024-03-26 20:25:19 +0100

    Prueba notificaciones locales funcionando

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,17 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'com.home.sync',
-  appName: 'home-sync',
-  webDir: 'www',
-  server: {
-    androidScheme: 'https',
-  },
-};
+    appId: 'com.home.sync',
+    appName: 'home-sync',
+    webDir: 'www',
+    server: {
+        androidScheme: 'https',
+    },
+    plugins: {
+        LocalNotifications: {
+            iconColor: '#488AFF',
+        },
+    },
+}
 
-export default config;
+export default config
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core'
+import {
+    CancelOptions,
+    Channel,
+    LocalNotifications,
+    ScheduleOptions,
+} from '@capacitor/local-notifications'
 
 @Component({
     selector: 'app-tab1',
@@ -7,4 +13,55 @@ import { Component } from '@angular/core'
 })
 export class Tab1Page {
     constructor() {}
+
+    scheduleNotification = async () => {
+        let options: ScheduleOptions = {
+            notifications: [
+                {
+                    id: 2,
+                    title: 'Título',
+                    body: 'Esto es el cuerpo',
+                    largeBody:
+                        'Esto es otro texto aún mas largo, para cuando se despliegue la notificación',
+                    summaryText: '¡Algo más!',
+                    largeIcon: 'res://mipmap-hdpi/ic_launcher_round',
+                    smallIcon: 'res://mipmap-hdpi/ic_launcher_round',
+                    // channelId: 'channel1'
+                },
+            ],
+        }
+
+        try {
+            await LocalNotifications.schedule(options)
+        } catch (error) {
+            alert(JSON.stringify(error))
+        }
+    }
+
+    cancelNotification = async () => {
+        let options: CancelOptions = {
+            notifications: [{ id: 1 }],
+        }
+        await LocalNotifications.cancel(options)
+    }
+
+    removeAllDeliveredNotifications = async () => {
+        await LocalNotifications.removeAllDeliveredNotifications()
+    }
+
+    // Sirve para crear varios modos de sonidos para las notificaciones
+    createChannel = async () => {
+        let chanel1: Channel = {
+            id: 'channel1',
+            description: 'first channel',
+            name: 'Channel 1',
+            visibility: 1,
+        }
+
+        try {
+            await LocalNotifications.createChannel(chanel1)
+        } catch (error) {
+            alert(JSON.stringify(error))
+        }
+    }
 }
